f2b9c0d0225bc4cf9d9156b6cdbd58ec3c308f0a
matyod <109255635+matyod@users.noreply.github.com>
2025-04-12 08:14:05 +0800

    UPDATE function scope

--- Scope/scope_lab.js
+++ Scope/scope_lab.js
@@ -13,3 +13,18 @@ const globalConst = "I'm a global constant";
 console.log(globalVar);
 console.log(globalLet);
 console.log(globalConst);
+
+console.log(blockVar);
+console.log(blockLet); // ReferenceError: blockLet only exists in the block scope
+console.log(blockConst); // ReferenceError: blockConst only exists in the block scope
+
+function show(){
+  var functionVar = "I'm a block-scoped var in a function";
+  let functionLet = "I'm a block-scoped let in a function";
+  const functionConst = "I'm a block-scoped const in a function";
+}
+
+show();
+console.log(functionVar); // ReferenceError: functionVar only exists in the function scope
+console.log(functionLet); // ReferenceError: functionLet only exists in the block scope
+console.log(functionConst); // ReferenceError: functionConst only exists in the block scope
