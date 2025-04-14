326c5d2940f89a25ea744ac4edc3704b40c88e1e
matyod <109255635+matyod@users.noreply.github.com>
2025-04-15 00:42:33 +0800

    ADD instruction

--- restaurantMenu/practice_task.js
+++ restaurantMenu/practice_task.js
@@ -1,3 +1,13 @@
+/*
+### Practice task
+1. You can include prices for all the items in breakfastMenu, mainCourseMenu, and dessertMenu.
+2. For this you can take include the prices as follows:
+```js
+const breakfastMenu = ['Pancakes- $12', 'Eggs Benedict -$22.99', 'Oatmeal -$21.99', 'Frittata -$15'];
+```
+3. Then, access it in the similar manner and you will see the output for the menu items along with prices as well.
+*/
+
 const breakfastMenu = ['Pancakes ($12)', 'Eggs Benedict ($22.99)', 'Oatmeal ($21.99)', 'Frittata ($15)'];
 const mainCourseMenu = ['Steak ($35)', 'Pasta ($18)', 'Burger ($14)', 'Salmon ($25)'];
 const dessertMenu = ['Cake ($8)', 'Ice Cream ($5)', 'Pudding ($7)', 'Fruit Salad ($6)'];
@@ -15,7 +25,7 @@ document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML
 
 // STEP 3: Defining forEach array method and for loop iteration
 let mainCourseItem = '';
-mainCourseMenu.forEach((item, index) => { 
+mainCourseMenu.forEach((item, index) => {
   mainCourseItem += `<p>Item ${index + 1}: ${item}</p>`;
 });
 
@@ -23,7 +33,7 @@ document.getElementById('maincourseMenuItems').innerHTML = mainCourseItem;
 
 // Iteration using for loop
 let dessertItem = '';
-dessertMenu.forEach((item, index) => { 
+dessertMenu.forEach((item, index) => {
   dessertItem += `<p>Item ${index + 1}: ${item}</p>`;
 });
 
